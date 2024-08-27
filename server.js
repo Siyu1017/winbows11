78ed8a2d5bb2bfd0efbcb897d664307dd822bc2d
Siyu1017 <adibao1111@gmail.com>
2024-08-27 18:58:28 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const app = express();
 
+app.get("/", function(req, res) {
+    return res.status(200).sendFile(__dirname + '/index.html');
+})
+
 app.use(express.static(__dirname + '/'), (req, res, next) => {
     res.status(404).redirect(`/?from=${encodeURIComponent(req.baseUrl + req.path)}`);
 })
