79c3616fdcdc640a94df6a393cb639e38c07951d
Siyu1017 <adibao1111@gmail.com>
2024-08-27 18:17:42 +0800

    Fix 404 redirect again...

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + '/'), (req, res, next) => {
+app.use((req, res, next) => {
     res.status(404).redirect(`/?from=${encodeURIComponent(req.baseUrl + req.path)}`);
 })
 
