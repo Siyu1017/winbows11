d608026f7370a1efe4f02b4d118a5aa6aea837c6
Siyu1017 <adibao1111@gmail.com>
2024-08-27 18:20:09 +0800

    Add vercel.json file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-    res.status(404).redirect(`/?from=${encodeURIComponent(req.baseUrl + req.path)}`);
+    res.status(404).redirect('/');
 })
 
 app.listen(3000, function () {
