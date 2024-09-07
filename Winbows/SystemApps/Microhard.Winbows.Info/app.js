cec3a5ee6e774e609a315ca273700c96a498f34e
Siyu1017 <adibao1111@gmail.com>
2024-09-07 23:48:09 +0800

    Update

---
+++ Winbows/SystemApps/Microhard.Winbows.Info/app.js
@@ -0,0 +1,3 @@
+;(async () => {
+    await System.requestAccessWindow('./window.js')
+})()
--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 'use strict';
 
 !(async () => {
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('./sw.js', { updateViaCache: 'none', scope: './' }).then((register) => {
-            register.update();
-            console.log('register success')
-        }, (err) => console.log('register fail:', err));
-    }
-
     Date.prototype.format = function (fmt) { var o = { "M+": this.getMonth() + 1, "d+": this.getDate(), "h+": this.getHours(), "m+": this.getMinutes(), "s+": this.getSeconds(), "q+": Math.floor((this.getMonth() + 3) / 3), "S": this.getMilliseconds() }; if (/(y+)/.test(fmt)) { fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length)); } for (var k in o) { if (new RegExp("(" + k + ")").test(fmt)) { fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length))); } } return fmt; }
 
     const path = {
