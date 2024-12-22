be546b18615999eef949da098ae73774362097f8
Siyu1017 <adibao1111@gmail.com>
2024-12-22 15:19:47 +0800

    Update

--- fs.js
+++ fs.js
@@ -1,8 +1,21 @@
 // File System
 !(async () => {
     const mainDisk = 'C';
-    const debuggerMode = false;
+    const debuggerMode = !!getJsonFromURL()['debug'] || false;
     const devMode = (getJsonFromURL()['dev'] || getJsonFromURL()['develop']) ? true : false;
+
+    function getStackTrace() {
+        var stack;
+
+        try {
+            throw new Error('');
+        } catch (error) {
+            stack = error.stack || '';
+        }
+
+        stack = stack.split('\n').map(function (line) { return line.trim(); });
+        return stack.splice(stack[0] == 'Error' ? 2 : 1);
+    }
 
     window.crashed = false;
     window.Crash = (err) => {
